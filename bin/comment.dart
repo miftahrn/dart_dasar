c278b98f2baad62441b964a02cedc505a5813b30
miftahrn <124230098@student.upnyk.ac.id>
2026-09-19 15:29:55 +0700

    komentar

---
+++ bin/comment.dart
@@ -0,0 +1,13 @@
+/// this is main function
+/// will excuted by dart
+void main() {
+  // this is name variable
+  var name = 'Miftah Rafli Nuryatama';
+
+  /**
+   * this is multi line comment
+   * you can add more here
+   */
+  print(name);
+
+}
