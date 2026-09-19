a579dd38a46fb2ca2a636ed2ec7af985fd515d78
miftahrn <124230098@student.upnyk.ac.id>
2026-09-19 20:08:27 +0700

    string

---
+++ bin/string.dart
@@ -0,0 +1,29 @@
+void main() {
+
+  String firstName = 'Miftah';
+  String lastName = "Nuryatama";
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName ${lastName}';
+  print(fullName);
+
+  var text = 'this is \'dart\' \$cool';
+  print(text);
+
+  var name1 = firstName + " " + lastName;
+  var name2 = 'Miftah' ' Rafli' ' Nuryatama';
+
+  print(name1);
+  print(name2);
+
+  var longString = '''
+this is long string
+multiline string
+learning dart
+  ''';
+
+  print(longString);
+
+}
