7c32084f9beed8717b99df4fe7133b8eb6ac0d32
miftahrn <124230098@student.upnyk.ac.id>
2026-09-22 13:38:32 +0700

    operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('Miftah' == 'Miftah');
+  print('Miftah' != 'Miftah');
+
+}
